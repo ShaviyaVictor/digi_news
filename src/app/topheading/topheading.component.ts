abf5df5af420a7824a012e08023674ebb54a6a6b
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-23 10:31:40 +0300

    Generate topheading component

---
+++ src/app/topheading/topheading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topheading',
+  templateUrl: './topheading.component.html',
+  styleUrls: ['./topheading.component.css']
+})
+export class TopheadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
